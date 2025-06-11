6dd932b122c99db4af7f7149380490501caafb10
JeromeTanguy22160 <jerometanguy@orange.fr>
2025-06-11 12:37:33 +0000

    étape 8 : delete address

---
+++ bre04-php-j6/delete-address.php
@@ -0,0 +1,14 @@
+<?php
+
+require "connexion.php";
+
+$id = $_POST["id"];
+
+$query = $db->prepare("DELETE FROM address WHERE id = :id");
+
+$parameters = [
+    "id" => $id,
+    ];
+
+$query->execute($parameters);
+?>
