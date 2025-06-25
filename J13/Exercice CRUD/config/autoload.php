cae6bc106f2b6e4bf5bb218c8e187855bf44617f
JeromeTanguy22160 <jerometanguy@orange.fr>
2025-06-25 11:54:59 +0000

    Add Etape 5

--- J13/Exercice CRUD/config/autoload.php
+++ J13/Exercice CRUD/config/autoload.php
@@ -1,7 +1,8 @@
 <?php
 
-require "models/User.php";
 require "controllers/UserController.php";
 require "config/Router.php";
+require "models/User.php";
+require "managers/AbstractManager.php";
 
 ?>
