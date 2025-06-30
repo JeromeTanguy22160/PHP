e18fe9fe2cc0d1125a9164bcaa74716a0860cea1
JeromeTanguy22160 <jerometanguy@orange.fr>
2025-06-30 15:07:56 +0000

    Add Fin Managers

--- J14/projet-blog-secu/managers/CommentManager.php
+++ J14/projet-blog-secu/managers/CommentManager.php
@@ -11,4 +11,45 @@ class CommentManager extends AbstractManager
         parent :: __construct();
     }
     
+    public function findByPost(int $postId) : array {
+        $query = $this -> db -> prepare ("SELECT comments.* FROM comments 
+        WHERE post_id = :postId");
+        $parameters = [
+            'postId' => $postId
+            ];
+        $query -> execute($parameters);
+        $commentsData = $query->fetchAll(PDO::FETCH_ASSOC);
+        $comments = [];
+        
+        foreach($commentsData as $commentData){
+            $comment = new Comment($commentData["content"], $commentData["user_id"], $commentData["post_id"]);
+            $comment -> setId($commentData["id"]);
+            $comments[] = $comment;
+        }
+        return $comments;
+    }
+    
+    public function create(Comment $comment) :void {
+        $query = $this -> db -> prepare("SELECT * FROM comments WHERE content = :content
+        AND user_id = :user_id AND post_id = :post_id");
+        $parameters = [
+        "content" => $comment ->getContent(),
+        "user_id" => $comment->getUser()->getId(),
+        "post_id" => $comment->getPost()->getId()
+        ];
+        $query -> execute($parameters);
+        $commentExist = $query -> fetch(PDO::FETCH_ASSOC);
+    
+        if (!$commentExist){
+          $query = $this -> db -> prepare("INSERT INTO comments(content, user_id, post_id) 
+          VALUES(:content,:user_id,:post_id)");
+          $parameters = [
+              "content" => $comment -> getContent(),
+              "user_id" => $comment -> getUser() -> getId(),
+              "post_id" => $comment -> getPost() -> getId()
+              ]; 
+          $query -> execute($parameters);
+        }
+        
+    }
 }
--- J14/projet-blog-secu/managers/UserManager.php
+++ J14/projet-blog-secu/managers/UserManager.php
@@ -11,5 +11,57 @@ class UserManager extends AbstractManager
         parent :: __construct();
     }
     
+    public function findByEmail(string $email) : ?User {
+            
+        $query = $this -> db -> prepare ("SELECT * FROM users 
+        WHERE users.email = :email");
+        $parameters = [
+            'email' => $email
+            ];
+        $query -> execute($parameters);
+        $user = $query->fetch(PDO::FETCH_ASSOC);
+        
+        return $user;
+    }
     
+    public function findOne(int $id) :?User {
+        
+        $query = $this -> db -> prepare("SELECT * FROM users WHERE id = :id");
+        $parameters = [
+        "id" => $id,
+        ];
+        $query -> execute($parameters);
+        $userData = $query->fetch(PDO::FETCH_ASSOC);
+        
+        if($userData){
+            $user = new User($userData["username"], $userData["email"], $userData["password"], $userData["role"]);
+            $user -> setId($userData["id"]);
+            $user -> setCreatedAt($userData["created_at"]);
+            return $user;
+        }
+        return null;
+    }
+    
+    public function create(User $user) :void {
+        $query = $this -> db -> prepare("SELECT * FROM users WHERE email = :email
+        AND username = :username");
+        $parameters = [
+        "email" => $user -> getEmail(),
+        "username" => $user -> getUsername()
+        ];
+        $query -> execute($parameters);
+        $userExist = $query -> fetch(PDO::FETCH_ASSOC);
+        
+        if (!$userExist){
+          $query = $this -> db -> prepare("INSERT INTO users(username, email, password, role) 
+          VALUES(:username, :email, :password, :role)");
+          $parameters = [
+              "username" => $user -> getUsername(),
+              "email" => $user -> getEmail(),
+              "password" => password_hash($user->getPassword(), PASSWORD_DEFAULT),
+              "role" => $user -> getRole() 
+              ]; 
+          $query -> execute($parameters);
+        }
+    }
 }
