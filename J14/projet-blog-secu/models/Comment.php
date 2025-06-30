192a6c5357c159a42e6832720e242112e5027ffa
JeromeTanguy22160 <jerometanguy@orange.fr>
2025-06-30 11:34:59 +0000

    Add fin des models

--- J14/projet-blog-secu/models/Comment.php
+++ J14/projet-blog-secu/models/Comment.php
@@ -4,8 +4,47 @@
  * @link : https://github.com/Gaellan
  */
 
+require "Post.php";
 
 class Comment
 {
+    private ? int $id = null;
+    
+    
+    public function __construct(
+        private string $content,
+        private User $user,
+        private Post $post
+        )
+    {
+        
+    }
 
+    public function getId() :? int{
+        return $this -> id;
+    }
+    public function setId($id) : void{
+        $this -> id = $id;
+    }
+    
+    public function getContent() : string{
+        return $this -> content;
+    }
+    public function setContent($content) : void{
+        $this -> content = $content;
+    }
+    
+    public function getUser() : User{
+        return $this -> user;
+    }
+    public function setUser(User $user) : void{
+        $this -> user = $user;
+    }
+    
+    public function getPost() : Post{
+        return $this -> post;
+    }
+    public function setPost(Post $post) : void{
+        $this -> post = $post;
+    }
 }
