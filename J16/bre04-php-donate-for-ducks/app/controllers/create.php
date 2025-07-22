0b507eb8d947815965d178b14eddbb2a63e99e3b
JeromeTanguy22160 <jerometanguy@orange.fr>
2025-07-22 11:51:03 +0000

    Add Etape 5

--- J16/bre04-php-donate-for-ducks/app/controllers/create.php
+++ J16/bre04-php-donate-for-ducks/app/controllers/create.php
@@ -2,6 +2,9 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/config');
+$dotenv->load();
+
 $sk = $_ENV['SK'];
 
 $stripe = new \Stripe\StripeClient($sk);
